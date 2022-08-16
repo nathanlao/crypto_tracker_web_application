f2d69ab4fe842a46530bd3078a5ad84575a61d70
nathan <75557717+nathanlao@users.noreply.github.com>
2022-08-15 23:41:03 -0700

    updated file

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Switch, Route, link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import Navbar from './components/Navbar';
+
+const App = () => {
+  return (
+    <div className="app">
+        <div className="navbar">
+
+        </div>
+        <div className="main">
+
+        </div>
+        <div className="footer">
+
+        </div>
+    </div>
+  );
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Button, Menu, Typography, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
+import { HomeOutlined, MoneyCollectutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+
+import icon from '../images/cryptocurrency.jpeg';
+
+const Navbar = () => {
+  return (
+    <div className="nav-container">
+        <div className="logo-container">
+            <Avatar src={icon} size="large"/>
+            <Typography.Title level={2} className="logo">
+                <link to="/">Crypto Tracker</link>
+
+            </Typography.Title>
+            {/* <Button className="menu-control-container">
+
+            </Button> */}
+        </div>     
+    </div>
+  );
+}
+
+export default Navbar
