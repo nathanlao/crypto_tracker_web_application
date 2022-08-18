8fe5d6bf8f480d78c11799d68d19f1be2dc68e28
nathan <75557717+nathanlao@users.noreply.github.com>
2022-08-18 00:39:19 -0400

    updated Switch

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Switch, Route, link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
-import { Navbar } from './components';
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails} from './components';
+import './App.css';
 
 const App = () => {
   return (
@@ -11,7 +12,27 @@ const App = () => {
             <Navbar />
         </div>
         <div className="main">
-
+            <Layout>
+                <div className="routes">
+                    <Switch>
+                        <Route exact path="/">
+                            <Homepage />
+                        </Route>
+                        <Route exact path="/exchanges">
+                            <Exchanges />
+                        </Route>
+                        <Route exact path="/cryptocurrencies">
+                            <Cryptocurrencies />
+                        </Route>
+                        <Route exact path="/crypto/:coinId">
+                            <CryptoDetails />
+                        </Route>
+                        <Route exact path="/news">
+                            <News />
+                        </Route>
+                    </Switch>
+                </div>
+            </Layout>
         </div>
         <div className="footer">
 
