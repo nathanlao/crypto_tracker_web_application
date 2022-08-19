09f7192086cb796df7c74d5b6a1d774ab723c61f
nathan <75557717+nathanlao@users.noreply.github.com>
2022-08-19 03:59:27 -0400

    Updated with footer secion

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, link } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
 import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails} from './components';
@@ -33,9 +33,17 @@ const App = () => {
                     </Switch>
                 </div>
             </Layout>
-        </div>
-        <div className="footer">
-
+            <div className="footer">
+                <Typography.Title level={5} style={{ color: 'white', textAlign: 'center', fontFamily: 'Monaco'}}>
+                    Crypto Tracker <br />
+                    All rights reserved
+                </Typography.Title>
+                <Space>
+                    <Link to="/">Home</Link>
+                    <Link to="/exchanges">Exchanges</Link>
+                    <Link to="/news">News</Link>
+                </Space>
+            </div>
         </div>
     </div>
   );
