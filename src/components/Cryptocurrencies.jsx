da30eac3e8353b5b97a2fa7383c18420f55f8496
nathan <75557717+nathanlao@users.noreply.github.com>
2022-08-20 03:08:26 -0400

    Added search bar

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import millify from 'millify';
 import { Link } from 'react-router-dom';
 import { Card, Row, Col, Input } from 'antd';
@@ -8,14 +8,29 @@ import { useGetCryptosQuery } from '../services/cryptoAPI';
 const Cryptocurrencies = ( {simplified} ) => {
   const count = simplified ? 10 : 100; // simplified is true, then set count to 10
   const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
-  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+  const [cryptos, setCryptos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    // Allow searching or filter out only the searched coin in the array(100 crypto)
+    const filteredData = cryptosList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()));
+
+    setCryptos(filteredData);
+
+  }, [cryptosList, searchTerm]); // useEffect() whenever cryptosList/searchTerm get changed
 
   if (isFetching) {
     return 'Loading...';
   }
 
   return (
-    <>
+    <>  
+        {!simplified && (
+          <div className="search-crypto">
+            <Input placeholder="Search Cryptocurrency" onChange={(e) => setSearchTerm(e.target.value)} />
+          </div>
+        )}
+        
         <Row gutter={[32, 32]} className="crypto-card-container">
           {cryptos?.map((currency) => ( // ? loop over including the undefined
               <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
