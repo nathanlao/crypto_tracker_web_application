f4f5ab30abdf53304f5131510a07eee854104278
nathan <75557717+nathanlao@users.noreply.github.com>
2022-08-20 04:38:23 -0400

    Updated coin.uuid

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -33,8 +33,8 @@ const Cryptocurrencies = ( {simplified} ) => {
         
         <Row gutter={[32, 32]} className="crypto-card-container">
           {cryptos?.map((currency) => ( // ? loop over including the undefined
-              <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
-                <Link to={`/crypto/${currency.id}`}>
+              <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.uuid}>
+                <Link to={`/crypto/${currency.uuid}`}>
                   <Card 
                     hoverable
                     title={`${currency.rank}. ${currency.name}`}
