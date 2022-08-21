0e3ed1a803cfaaf138be7d09a14c5e2583a89edf
nathan <75557717+nathanlao@users.noreply.github.com>
2022-08-21 01:46:44 -0400

    Updated cryptoDetails with useParams() testing

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import HTMLReactParser from 'html-react-parser';
+import { useParams } from 'react-router-dom';
+import millify from 'millify';
+import { Row, Col, Typography, Select } from 'antd';
+
+import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, 
+         TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
+
+const { Title, Text } = Typography;
+const { Option } = Select;
 
 const CryptoDetails = () => {
+  const { coinID } = useParams(); // take the id of the url as variables
+
   return (
     <div>
-     CryptoDetails
+     CryptoDetails {coinID}
     </div>
   );
 }
