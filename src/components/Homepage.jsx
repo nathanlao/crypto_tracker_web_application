d954e0766363cd97cf6b7ab1d82099619e34d67f
nathan <75557717+nathanlao@users.noreply.github.com>
2022-08-22 17:49:10 -0400

    Updated font for each col

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -20,21 +20,21 @@ const Homepage = () => {
 
   return (
     <>
-        <Title level={2} className="heading">
+        <Title level={2} className="heading" style={{fontFamily:'Comic Sans MS'}}>
           Global Crypto Stats
         </Title>
         <Row>
-            <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} valueStyle={{fontFamily:'Comic Sans MS'}} /></Col>
-            <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} valueStyle={{fontFamily:'Comic Sans MS'}} /></Col>
-            <Col span={12}><Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)} valueStyle={{fontFamily:'Comic Sans MS'}}/></Col>
-            <Col span={12}><Statistic title="Total 24h Volumn" value={millify(globalStats.total24hVolume)} valueStyle={{fontFamily:'Comic Sans MS'}}/></Col>
-            <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} valueStyle={{fontFamily:'Comic Sans MS'}}/></Col>
+            <Col span={12}><Statistic style={{fontFamily:'Comic Sans MS'}} title="Total Cryptocurrencies" value={globalStats.total} valueStyle={{fontFamily:'Comic Sans MS'}} /></Col>
+            <Col span={12}><Statistic style={{fontFamily:'Comic Sans MS'}} title="Total Exchanges" value={millify(globalStats.totalExchanges)} valueStyle={{fontFamily:'Comic Sans MS'}} /></Col>
+            <Col span={12}><Statistic style={{fontFamily:'Comic Sans MS'}} title="Total Market Cap" value={millify(globalStats.totalMarketCap)} valueStyle={{fontFamily:'Comic Sans MS'}}/></Col>
+            <Col span={12}><Statistic style={{fontFamily:'Comic Sans MS'}} title="Total 24h Volumn" value={millify(globalStats.total24hVolume)} valueStyle={{fontFamily:'Comic Sans MS'}}/></Col>
+            <Col span={12}><Statistic style={{fontFamily:'Comic Sans MS'}} title="Total Markets" value={millify(globalStats.totalMarkets)} valueStyle={{fontFamily:'Comic Sans MS'}}/></Col>
         </Row>
         <div className="home-heading-container">
-            <Title level={2} className="home-title">
+            <Title level={2} className="home-title" style={{fontFamily:'Comic Sans MS'}}>
               Top 10 Cryptocurrencies in the world
             </Title>
-            <Title level={3} className="show-more">
+            <Title level={3} className="show-more" style={{fontFamily:'Comic Sans MS'}}>
                 <Link to="./cryptocurrencies">
                   Show More
                 </Link>
@@ -43,10 +43,10 @@ const Homepage = () => {
         <Cryptocurrencies simplified/>
 
         <div className="home-heading-container">
-            <Title level={2} className="home-title">
+            <Title level={2} className="home-title" style={{fontFamily:'Comic Sans MS'}}>
               Latest Crypto News
             </Title>
-            <Title level={3} className="show-more">
+            <Title level={3} className="show-more" style={{fontFamily:'Comic Sans MS'}}>
                 <Link to="./news">
                   Show More
                 </Link>
