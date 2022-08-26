32092dee2906be543c40da19aeeb6a1e2e00d076
nathan <75557717+nathanlao@users.noreply.github.com>
2022-08-26 00:20:04 -0400

    Handle LineChart undefined value

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -61,11 +61,11 @@ const CryptoDetails = () => {
                 className="select-timeperiod" 
                 placeholder="Select Time Period"
                 onChange={(value) => setTimePeriod(value)}
-            >
+        >
               {time.map((date) => <Option key={date}>{date}</Option> )}
         </Select>
 
-        <LineChart coinHistory= {coinHistory} currentPrice={millify(cryptoDetails?.price)} coinName={cryptoDetails?.name}/>
+        <LineChart coinHistory={coinHistory} currentPrice={millify(cryptoDetails?.price)} coinName={cryptoDetails?.name}/>
 
             <Col className="stats-container">
                 <Col className="coin-value-statistics">
