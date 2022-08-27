1f92bfaa059bab4fb56000a62ec72e955355f4c3
nathan <75557717+nathanlao@users.noreply.github.com>
2022-08-27 16:48:30 -0400

    Added active button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
@@ -6,6 +6,27 @@ import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOut
 import icon from '../images/crypto.webp';
 
 const Navbar = () => {
+  const [activeButton, setActiveButon] = useState(true);
+  const [screenSize, setScreenSize] = useState(null);
+  
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+    
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  useEffect(() => {
+    if(screenSize < 768) {
+        setActiveButon(false);
+    } else {
+        setActiveButon(true);
+    }
+  }, [screenSize]);
+
   return (
     <div className="nav-container">
         <div className="logo-container">
@@ -13,8 +34,14 @@ const Navbar = () => {
             <Typography.Title level={3} className="logo" italic>
                 <Link to="/">Crypto Tracker</Link> 
             </Typography.Title>
+            
+            <Button className="menu-control-container" onClick={() => setActiveButon(!activeButton)}>
+                <MenuOutlined />
+            </Button>
+
         </div>
-        {/* <Menu theme="dark">
+        {/* {activeMenu && (
+        <Menu theme="dark">
             <Menu.Item icon={<HomeOutlined />}>
                 <Link to="/">Home</Link>
             </Menu.Item>
@@ -27,27 +54,32 @@ const Navbar = () => {
             <Menu.Item icon={<BulbOutlined />}>
                 <Link to="/news">News</Link>
             </Menu.Item>
-        </Menu>      */}
-        <Button type="link" style={{ color: 'white' }} icon={<HomeOutlined />} shape="round" block>
-            <Link to="/">   Home</Link>
-        </Button>
-        <br />
-        <br />
-        <Button type="link" style={{ color: 'white' }} icon={<FundOutlined />} shape="round" block>
-            <Link to="/Cryptocurrencies">   Cryptocurrencies</Link>
-        </Button>
-        <br />
-        <br />
-        <Button type="link" style={{ color: 'white' }} icon={<MoneyCollectOutlined />} shape="round" block>
-            <Link to="/exchanges">   Exchanges</Link>
-        </Button>
-        <br />
-        <br />
-        <Button type="link" style={{ color: 'white' }} icon={<BulbOutlined />} shape="round" block>
-            <Link to="/news">   News</Link>
-        </Button>
+        </Menu>   
+        )}   */}
+        {activeButton && (
+            <>
+            <Button type="link" style={{ backgroundColor: '#130F40', color: 'white'}} icon={<HomeOutlined />} shape="round" block>
+                <Link to="/">   Home</Link>
+            </Button>
+            <br />
+            <br />
+            <Button type="link" style={{ backgroundColor: '#130F40', color: 'white' }} icon={<FundOutlined />} shape="round" block>
+                <Link to="/Cryptocurrencies">   Cryptocurrencies</Link>
+            </Button>
+            <br />
+            <br />
+            <Button type="link" style={{ backgroundColor: '#130F40', color: 'white' }} icon={<MoneyCollectOutlined />} shape="round" block>
+                <Link to="/exchanges">   Exchanges</Link>
+            </Button>
+            <br />
+            <br />
+            <Button type="link" style={{ backgroundColor: '#130F40', color: 'white' }} icon={<BulbOutlined />} shape="round" block>
+                <Link to="/news">   News</Link>
+            </Button>
+            </>
+        )}
     </div>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
