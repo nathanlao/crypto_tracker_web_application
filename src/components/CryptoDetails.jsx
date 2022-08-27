062558aeee418fddb6e605d3e36a6254c2dcaa63
nathan <75557717+nathanlao@users.noreply.github.com>
2022-08-27 17:28:57 -0400

    Updated loader

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -10,6 +10,7 @@ import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCi
 import { useGetCryptoDetailsQuery, useGetCryptoHistoryQuery} from '../services/cryptoAPI';
 
 import LineChart from './LineChart';
+import Loader from './Loader';
 
 const { Title, Text } = Typography;
 const { Option } = Select;
@@ -24,7 +25,7 @@ const CryptoDetails = () => {
   // console.log(data);
 
   if (isFetching) {
-    return 'Loading';
+    return <Loader />;
   }
 
   const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
